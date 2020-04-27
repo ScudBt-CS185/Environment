8f24a839a8e627904376f1884eb5ad958d39a6e4
ScudBt <43557476+ScudBt@users.noreply.github.com>
2020-04-27 02:49:09 -0700

    4 fixing top

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-//Get the button
-var mybutton = document.getElementById("myBtn");
-
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 25) {
+    var mybutton = document.getElementById("myBtn");
+    mybutton.style.color = "black";
+    //if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    if (window.scrollY > screen.height / 4) {
         mybutton.style.display = "block";
     } else {
         mybutton.style.display = "none";
@@ -14,6 +14,12 @@ function scrollFunction() {
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
+    console.log(window.scrollY);
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
+    console.log(screen.height / 4);
+    console.log(window.scrollY);
 }
+
+
+
